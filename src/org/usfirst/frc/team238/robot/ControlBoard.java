806a208360f781b81f2b5ae16ed4707948eb28d2
sean <sean@sean-HP>
2015-01-21 19:27:06 -0500

    Updated ControlBoard so that all functions are static.

--- src/org/usfirst/frc/team238/robot/ControlBoard.java
+++ src/org/usfirst/frc/team238/robot/ControlBoard.java
@@ -5,14 +5,16 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
 public class ControlBoard { //Here are the joysticks for controlling the robot
-	Joystick joy1;  
-	Joystick joy2;
+	static Joystick joy1;  
+	static Joystick joy2;
+	
 	public void controlBoardInit()
 	{
 		try
 		{
 			joy1 = new Joystick(1);
 			joy2 = new Joystick(2);
+			
 		}
 		
 		catch(Exception ex)
@@ -21,74 +23,118 @@ public class ControlBoard { //Here are the joysticks for controlling the robot
 		}
 	}
 	// This is Button 1, it makes the claws suck things in
-	public boolean isTriggerPressed()
+	public static boolean isTriggerPressed()
 	{
-		//modified
-		boolean trigger = joy1.getRawButton(1);
-		SmartDashboard.putString("Button 1", "Button 1 Is Presses");
-		return trigger;
+		boolean trigger = false;
+		
+		try
+		{
+			trigger = joy1.getRawButton(1);
+			SmartDashboard.putString("Button 1", "Button 1 Is Initialized");
+			return trigger;
+		}
+		
+		catch(Exception ex)
+		{
+			System.out.println("Not Working");
+			return trigger;
+		}
+		
 	}
 	// This is Button 2, it makes the claws spit things out
-	public boolean isButtonTwoPressed()
+	public static boolean isButtonTwoPressed()
 	{
 		boolean trigger = joy1.getRawButton(2);
-		SmartDashboard.putString("Button 2", "Button 2 Is Presses");
+		SmartDashboard.putString("Button 2", "Button 2 Is Initialized");
 		return trigger;
 	}
 	// This is Button 3, it will make the claws spin items left
-	public boolean isButtonThreePressed()
+	public static boolean isButtonThreePressed()
 	{
 		boolean trigger = joy1.getRawButton(3);
-		SmartDashboard.putString("Button 3", "Button 3 Is Presses");
+		SmartDashboard.putString("Button 3", "Button 3 Is Initialized");
 		return trigger;
 	}
 	// This is Button 4, it will make the claws spin items right
-	public boolean isButtonFourPressed()
+	public static boolean isButtonFourPressed()
 	{
 		boolean trigger = joy1.getRawButton(4);
-		SmartDashboard.putString("Button 4", "Button 4 Is Presses");
+		SmartDashboard.putString("Button 4", "Button 4 Is Initialized");
 		return trigger;
 	}
 	// This is Button 5, it will open the Saloon Doors
-	public boolean isButtonFivePressed()
+	public static boolean isButtonFivePressed()
 	{
 		boolean trigger = joy1.getRawButton(5);
-		SmartDashboard.putString("Button 5", "Button 5 Is Presses");
+		SmartDashboard.putString("Button 5", "Button 5 Is Initialized");
 		return trigger;
 	}
 	// This is Button 6, it will close the Saloon Doors
-	public boolean isButtonSixPressed()
+	public static boolean isButtonSixPressed()
 	{
 		boolean trigger = joy1.getRawButton(6);
-		SmartDashboard.putString("Button 6", "Button 6 Is Presses");
+		SmartDashboard.putString("Button 6", "Button 6 Is Initialized");
 		return trigger;
 	}
 	
-	public boolean isJoy2ButtonOnePresses()
+	public static boolean isJoy2ButtonOnePresses()
 	{
 		boolean trigger = joy2.getRawButton(1);
-		SmartDashboard.putString("Joy2 Button 1", "Button 1 Is Presses");
+		SmartDashboard.putString("Joy2 Button 1", "Button 1 Is Initialized");
 		return trigger;
 	}
 	
-	public boolean isJoy2ButtonTwoPresses()
+	public static boolean isJoy2ButtonTwoPresses()
 	{
 		boolean trigger = joy2.getRawButton(2);
-		SmartDashboard.putString("Joy2 Button 2", "Button 2 Is Presses");
+		SmartDashboard.putString("Joy2 Button 2", "Button 2 Is Initialized");
 		return trigger;
 	}
 	
-	public boolean isJoy2ButtonThreePresses()
+	public static boolean isJoy2ButtonThreePresses()
 	{
 		boolean trigger = joy2.getRawButton(3);
-		SmartDashboard.putString("Joy2 Button 3", "Button 3 Is Presses");
+		SmartDashboard.putString("Joy2 Button 3", "Button 3 Is Initialized");
 		return trigger;
 	}
 	
-	public boolean isJoy2ButtonFourPresses()
+	public static boolean isJoy2ButtonFourPresses()
 	{
 		boolean trigger = joy2.getRawButton(4);
-		SmartDashboard.putString("Joy2 Button 4", "Button 4 Is Presses");
+		SmartDashboard.putString("Joy2 Button 4", "Button 4 Is Initialized");
 		return trigger;
+	}
+	
+	public void controlBoardTest()
+	{
+		boolean buttonOne = isTriggerPressed();
+    	SmartDashboard.putBoolean("TestCB", buttonOne);
+    	
+    	boolean buttonTwo = isButtonTwoPressed();
+    	SmartDashboard.putBoolean("TestCB", buttonTwo);
+    	
+    	boolean buttonThree = isButtonThreePressed();
+    	SmartDashboard.putBoolean("TestCB", buttonThree);
+    	
+    	boolean buttonFour = isButtonFourPressed();
+    	SmartDashboard.putBoolean("TestCB", buttonFour);
+    	
+    	boolean buttonFive = isButtonFivePressed();
+    	SmartDashboard.putBoolean("TestCB", buttonFive);
+    	
+    	boolean buttonSix = isButtonSixPressed();
+    	SmartDashboard.putBoolean("TestCB", buttonSix);
+    	
+    	boolean joyTwoButtonOne = isButtonSixPressed();
+    	SmartDashboard.putBoolean("TestCB", joyTwoButtonOne);
+    	
+    	boolean joyTwoButtonTwo = isButtonSixPressed();
+    	SmartDashboard.putBoolean("TestCB", joyTwoButtonTwo);
+    	
+    	boolean joyTwoButtonThree = isButtonSixPressed();
+    	SmartDashboard.putBoolean("TestCB", joyTwoButtonThree);
+    	
+    	boolean joyTwoButtonFour = isButtonSixPressed();
+    	SmartDashboard.putBoolean("TestCB", joyTwoButtonFour);
 	}
 }
