174a1bc285ded2f7197cbb3ff3fe829c6b882aeb
Team238 <Team238@Team238-PC1>
2015-01-21 19:11:00 -0500

    initial main codeline check in

---
+++ src/org/usfirst/frc/team238/robot/CrusaderCommon.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team238.robot;
+
+public class CrusaderCommon {
+
+	public final static double CLAWMOTORSPEED = 0.5;
+	public final static double MOTOROFF = 0;
+	public final static double CLAWMOTORSPEEDREVERSE = -0.5;
+	public final static double DOORMOTORSPEED = 0.5;
+	public final static double DOORMOTORSPEEDREVERSE = -0.5;
+
+}
+
+
