37940207a6840ebf4525c5bb418b7714a4786501
sean <sean@sean-HP>
2015-01-21 19:30:02 -0500

    Updated SaloonDoors with new functionality, including use of CrusaderCommon.

--- src/org/usfirst/frc/team238/robot/SaloonDoors.java
+++ src/org/usfirst/frc/team238/robot/SaloonDoors.java
@@ -1,46 +1,69 @@
 package org.usfirst.frc.team238.robot;
 
-import edu.wpi.first.wpilibj.Relay;
-import edu.wpi.first.wpilibj.Relay.Value;
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class SaloonDoors {
 
-	Relay LeftDoorMotor;
-	Relay RightDoorMotor;
-	ControlBoard theControlBoard;
+	Talon leftDoorMotor;
+	Talon rightDoorMotor;
+	AnalogPotentiometer leftPot;
+	AnalogPotentiometer rightPot;
 	
 	/*OVerview
 	 * This function initializes Relays: Left Door and Right Door.
 	 * Input: none 
 	 * Output: none  
 	 */
-	public void Init(ControlBoard controlboard) {
+	public void Init() {
 			
 		try {
-			theControlBoard = new ControlBoard();
-			LeftDoorMotor = new Relay(1, Relay.Direction.kForward);
+			
+			leftDoorMotor = new Talon(8);
 			SmartDashboard.putString("LeftDoorMotor", "Initialized");
-			RightDoorMotor = new Relay(2, Relay.Direction.kReverse);
+			
+			rightDoorMotor = new Talon(9);
 			SmartDashboard.putString("RightDoorMotor", "Initialized");
+			
+			leftPot = new AnalogPotentiometer(90);
+			SmartDashboard.putString("Leftpot","Initialized");
+			
+			rightPot = new AnalogPotentiometer(90);
+			SmartDashboard.putString("Rightpot","Initialized");
+			
+			SmartDashboard.putString("theControlBoard","Initialized");
+			
 		} catch (Exception ex) {
-			System.out.println(ex.getMessage());
+			System.out.println(ex.getStackTrace());
 		}
 	}
 	
 	/* Overview
 	 * When the OpenSaloondoorsButton is pressed the motors will 
-	 * turn forward, opening the Saloon Doors until sensor says stop. 
+	 * turn forward, opening the Saloon Doors. 
 	 * Input: OpenSaloonDoorsButton
 	 * Output: Motors Forward, "Saloon Doors Open"
 	 */
 	private void OpenDoors() {
-		RightDoorMotor.set(Value.kForward);
-		SmartDashboard.putString("RightDoorMotor", "kForward");
-		LeftDoorMotor.set(Value.kForward);
-		SmartDashboard.putString("LeftDoorMotor", "kForward");
+		String outPutValue = "";
+		
+		if ((leftPot.get() == 90) && (rightPot.get() == 90)) {
+
+			rightDoorMotor.set(CrusaderCommon.DOORMOTORSPEED);
+			leftDoorMotor.set(CrusaderCommon.DOORMOTORSPEED);
+			outPutValue = ("CrusaderCommon.DOORMOTORSPEED");
+			
+		}
+		else {
+			leftDoorMotor.set(CrusaderCommon.MOTOROFF);
+			rightDoorMotor.set(CrusaderCommon.MOTOROFF);
+			outPutValue = "CrusaderCommon.MOTOROFF";
+		} 
+		
+		SmartDashboard.putString("SaloonDoors.OpenDoors", outPutValue);
 	}	
-	
+
 	/* Overview
 	 * When the CloseSaloondoorsButton is pressed the motors will 
 	 * turn backward, closing the Saloon Doors. 
@@ -48,10 +71,21 @@ public class SaloonDoors {
 	 * Output: Motors Backward, "Saloon Doors Close"
 	 */
 	private void CloseDoors() {
-		RightDoorMotor.set(Value.kReverse);
-		SmartDashboard.putString("RightDoorMotor", "kReverse");
-		LeftDoorMotor.set(Value.kReverse);
-		SmartDashboard.putString("LeftDoorMotor", "kReverse");
+		String outPutValue = "";
+
+		if ((leftPot.get() == 90) && (rightPot.get() == 90)) {
+
+			rightDoorMotor.set(CrusaderCommon.DOORMOTORSPEEDREVERSE);
+			leftDoorMotor.set(CrusaderCommon.DOORMOTORSPEEDREVERSE);
+			outPutValue = "CrusaderCommon.DOORMOTORSPEEDREVERSE";
+		}
+		else { 
+			leftDoorMotor.set(CrusaderCommon.MOTOROFF);
+			rightDoorMotor.set(CrusaderCommon.MOTOROFF);
+			outPutValue = "CrusaderCommon.MOTOROFF";
+		}
+
+		SmartDashboard.putString ("SaloonDoors.CloseDoors",outPutValue);
 	}
 
 	/*Overview
@@ -61,20 +95,27 @@ public class SaloonDoors {
 	 * Output: none       
 	 */
 	public void poll() {
-		boolean b1 = theControlBoard.isButtonFivePressed(); //open door
-		boolean b2 = theControlBoard.isButtonSixPressed(); // close door
-		SmartDashboard.putBoolean("b1", b1);
-		SmartDashboard.putBoolean("b2", b2);
+		boolean b1 = ControlBoard.isButtonFivePressed(); //open door
+		SmartDashboard.putString("ButtonFive", "Pressed");
+		boolean b2 = ControlBoard.isButtonSixPressed(); // close door
+		SmartDashboard.putString("ButtonSix", "Pressed");
 		if ((b1 == true) && (b2 == true)) {
 			// do nothing
 		} 
-		else if (b1 == true)
-		{
+		else if (b1 == true) {
 			OpenDoors();
 		} 
 		else if (b2 == true) {
 			CloseDoors();
-		} else { // do nothing
+		}
+		else if ((b1 == false) && (b2 == false))
+		{
+			leftDoorMotor.set(CrusaderCommon.MOTOROFF);
+			rightDoorMotor.set(CrusaderCommon.MOTOROFF);
+			SmartDashboard.putString("Button 5 or 6", "motors are off");
+		
+		} else { 
+			SmartDashboard.putString("Button 5 or 6", "not pressed");
 		}
 	}
 }	
