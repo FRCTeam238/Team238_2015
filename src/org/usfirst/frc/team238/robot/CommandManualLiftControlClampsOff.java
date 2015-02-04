cd42b17af114f7b22b2e45315832701628d9357e
sean <sean@sean-HP>
2015-02-03 20:40:43 -0500

    Made CommandManualLiftControlClampsOff

--- src/org/usfirst/frc/team238/robot/CommandManualLiftControlClampsOff.java
+++ src/org/usfirst/frc/team238/robot/CommandManualLiftControlClampsOff.java
@@ -1,5 +1,23 @@
 package org.usfirst.frc.team238.robot;
 
-public class CommandManualLiftControlClampsOff {
+public class CommandManualLiftControlClampsOff implements Command 
+{
+	Lift myLift;
+	SaloonDoors mySaloonDoors;
 
+	public CommandManualLiftControlClampsOff(Lift theLift, SaloonDoors theSaloonDoors)
+	{
+		this.myLift = theLift;
+		this.mySaloonDoors = theSaloonDoors;
+	}
+
+	public void execute()
+	{
+		myLift.manualControlOfLifter(0);
+		//mySaloonDoors.CloseDoors();
+
+	}
+
+	public void execute(double overRideValue){
+	}
 }
